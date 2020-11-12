cd167ab4b44d51be1f93c86275ae76e3a16559e4
Carlos <carlosmanuelcomenares@gmail.com>
2020-11-12 14:44:38 -0500

    subida de archivos

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,5 +10,8 @@ app.use(require('./login'));
 
 app.use(require('./categoria'));
 app.use(require('./producto'));
+app.use(require('./uploads'));
+
+app.use(require('./imagenes'));
 
 module.exports = app;
